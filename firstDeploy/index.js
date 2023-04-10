c6a53fedd892c423c7452f4f305f824a641292a9
YAQOT <63197328+YAQ0T@users.noreply.github.com>
2023-04-10 20:37:46 +0300

    motaz

--- firstDeploy/index.js
+++ firstDeploy/index.js
@@ -1,14 +1,18 @@
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
+  const url = req.url;
+
   if (req.url == "/motaz") {
     res.writeHead(200);
     res.end(
-      `hello to my best borther in the life and to the best TARIK ${req.url}`
+      `hello to my best borther in the life and to the best TARIK ${url.slice(
+        1
+      )}`
     );
   } else {
     res.writeHead(200);
-    res.end(`Hello To me first deploy in my life ${req.url}`);
+    res.end(`Hello To me first deploy in my life ${url.slice(1)}`);
   }
 });
 const port = process.env.PORT || 3000;
